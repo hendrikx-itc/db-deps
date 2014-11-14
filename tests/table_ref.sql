55a0e2d066bbd82f23c8737f5ff8ae1055f40d5c
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-11-14 14:42:43 +0100

    Remove erroneous copy-pasted comment

--- tests/table_ref.sql
+++ tests/table_ref.sql
@@ -13,8 +13,6 @@ CREATE TABLE dummy (
 --------
 SELECT plan(1);
 
--- PostgreSQL does some funky formatting with the view query, so there is some
--- unexpected spacing here as well.
 SELECT is(
     (dep_recurse.table_ref('public', 'dummy'))::text,
     'public.dummy'
